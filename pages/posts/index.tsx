874f3949b998829b278b5c5449e327a126fe3b67
zarocknc <sebastian_nicolaschong@protonmail.com>
2023-02-07 14:38:19 -0500

    early deploy test

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -3,27 +3,34 @@ import { trpc } from "@/utils/trpc";
 export default function PostsPage() {
   const data = trpc.posts.listPosts.useQuery();
   return (
-    <main className="flex-col">
-      {data.data?.posts.map((post) => {
-        return (
-          <div key={post.id} className="flex bg-slate-200 shadow-xl m-6">
-            <figure>
-              <img
-                src={post.author.image ?? "@/public/favicon"}
-                alt={post.title}
-              />
-            </figure>
+    <main>
+      <div className="container mx-auto ">
+        {data.data?.posts.map((post) => {
+          return (
+            <div
+              key={post.id}
+              className="flex flex-row bg-slate-200 shadow-xl m-6 border-8 border-orange-500"
+            >
+              <div className="">
+                {/* eslint-disable-next-line @next/next/no-img-element*/}
+                <img
+                  src={post.author.image ?? "@/public/favicon"}
+                  alt={post.author.name as string}
+                />
+                <h1>{post.author.name}</h1>
+              </div>
 
-            <div className="card-body">
-              <h2 className="card-title">{post.title}</h2>
-              <p>{post.content}</p>
-              <div className="card-actions justify-end">
-                <button className="btn btn-primary">Comentar</button>
+              <div className="col-span-12 flex-col">
+                <h1 className="text-2xl">{post.title}</h1>
+                <p className="text-base">{post.content}</p>
+                <div className="">
+                  <button className="btn btn-primary">Comentar</button>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </main>
   );
 }
